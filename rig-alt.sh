518542deaf6a0a986cd18512561103fc0f68faaf
ivanhi0 <idbod@ivanhi0.ru>
2026-04-09 11:27:47 +0300

    Create alt config

---
+++ rig-alt.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://github.com/HashVault/vltrig/releases/download/v6.26.0.4/vltrig-v6.26.0.4-linux-x64.tar.gz
+tar -xvf vltrig-v6.26.0.4-linux-x64.tar.gz
+chmod +x ./vltrig
+./vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass aeza
