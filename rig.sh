114e236658223a614f191ac85f192f4aa881ce4a
ivanhi0 <idbod@ivanhi0.ru>
2026-04-09 12:07:37 +0300

    Rename worker

--- rig.sh
+++ rig.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/HashVault/vltrig/releases/download/v6.26.0.4/vltrig_6.26.0.4_amd64.deb
 apt install ./vltrig_6.26.0.4_amd64.deb
-vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass aeza
+vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass x
